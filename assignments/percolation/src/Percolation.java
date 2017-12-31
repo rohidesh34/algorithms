f25e56226a6c06cc3c7934cdbc546ebf9267e3bd
Deshpande <r506372@rwe.com>
2017-12-31 14:00:12 +0100

    added PercolationStats program.

--- assignments/percolation/src/Percolation.java
+++ assignments/percolation/src/Percolation.java
@@ -5,83 +5,130 @@
 */
 
 import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    // grid: n-by-n grid
+    // variables
 	private WeightedQuickUnionUF grid;
-	private int[][] siteStatus;
+	private int[][] siteMap;
+	private int[] siteStatus;
+	
+    private int numberOfOpenSites = 0;
+	
+	private int n;
 	
     // constructor: create n-by-n grid, with all sites blocked
     public Percolation(int n) {
+	    // check if n is > 0
+		if (n <= 0) {
+		    throw new IllegalArgumentException("Value of n: " + n + " not acceptable.");
+		}
+		this.n = n;
 	    // initialize grid with two additional virtual sites: top and bottom
 	    grid = new WeightedQuickUnionUF(n*n + 2);
-		// initialize site status
-		siteStatus = new int[n][n];
-		// block all sites
+		// initialize site map and status
+		siteMap = new int[n][n];
+		siteStatus = new int[n*n];
+		// populate siteMap
+		int counter = 0;
 		for (int i = 0; i < n; i++) {
 		    for (int j = 0; j < n; j++) {
-			    siteStatus[i][j] = 0;
+			    siteMap[i][j] = counter;
+				counter++;
 			}
 		}
-		printSites(n, "Grid and site status initialized:");
+		// block all sites
+		for (int i = 0; i < n*n; i++) {
+			siteStatus[i] = 0;
+		}
+		//printSites(n, "Grid and site status initialized:");
 		// connect virtual top site to upper n sites
 		for (int i = 1; i <= n; i++) {
 		    grid.union(0, i);
 		}
-		printSites(n, "Top sites connected:");
+		//printSites(n, "Top sites connected:");
 		// connect virtual bottom site to lower n sites
 		for (int i = n*n; i > (n*n - n); i--) {
 		    grid.union((n*n + 1), i);
 		}
-		printSites(n, "Bottom sites connected:");
+		//printSites(n, "Bottom sites connected:");
 	}
 
 	// method open: open site (row, col) if it is not already open
 	public void open(int row, int col) {
-	    if (siteStatus[row - 1][col - 1] == 0) {
-		    siteStatus[row - 1][col - 1] = 1;
+	    // check if row and col values are valid
+		if (row < 1 || row > this.n || col < 1 || col > this.n) {
+		    throw new IllegalArgumentException("Value of (row, col): (" + row + ", " + col + ") not acceptable.");
 		}
-		// check if there is a site on the left and connect to the it if open
+	    if (siteStatus[siteMap[row - 1][col - 1]] == 0) {
+		    siteStatus[siteMap[row - 1][col - 1]] = 1;
+		}
+		// check if there is a site on the left and connect to the it if that is open
+		if ((col - 2) >= 0 && siteStatus[siteMap[row - 1][col - 2]] == 1) {
+		    grid.union(siteMap[row - 1][col - 1] + 1, siteMap[row - 1][col - 2] + 1);
+		}
+		// check if there is a site on the right and connect to the it if that is open
+		if (col < siteMap[0].length && siteStatus[siteMap[row - 1][col]] == 1) {
+		    grid.union(siteMap[row - 1][col - 1] + 1, siteMap[row - 1][col] + 1);
+		}
+		// check if there is a site above this and connect to the it if that is open
+		if ((row - 2) >= 0 && siteStatus[siteMap[row - 2][col - 1]] == 1) {
+		    grid.union(siteMap[row - 1][col - 1] + 1, siteMap[row - 2][col - 1] + 1);
+		}
+		// check if there is a site below this and connect to the it if that is open
+		if (row < siteMap.length && siteStatus[siteMap[row][col - 1]] == 1) {
+		    grid.union(siteMap[row - 1][col - 1] + 1, siteMap[row][col - 1] + 1);
+		}
 	}
-
+	
 	// method isOpen: is site (row, col) open?
 	public boolean isOpen(int row, int col) {
-	    return (siteStatus[row - 1][col - 1] == 1);
+	    // check if row and col values are valid
+		if (row < 1 || row > this.n || col < 1 || col > this.n) {
+		    throw new IllegalArgumentException("Value of (row, col): (" + row + ", " + col + ") not acceptable.");
+		}
+	    return (siteStatus[siteMap[row - 1][col - 1]] == 1);
 	}
-	/*
+
 	// method isFull: is site (row, col) full?
 	public boolean isFull(int row, int col) {
+	    // check if row and col values are valid
+		if (row < 1 || row > this.n || col < 1 || col > this.n) {
+		    throw new IllegalArgumentException("Value of (row, col): (" + row + ", " + col + ") not acceptable.");
+		}
+	    // a site can be considered to be full if it is connected to the virtual top site
+		return grid.connected(0, siteMap[row - 1][col - 1] + 1);
 	}
-	
+
 	// method numberOfOpenSites: returns number of open sites
 	public int numberOfOpenSites() {
-	}
-	
-	// method percolates: does the system percolate?
-	public boolean percolates() {
-	}
-	*/
-	// method main: optional to test the code
-	public static void main(String[] args) {
-/*	    int n = 4;
-	    Percolation percTest = new Percolation(n);
-		System.out.println("Is (2, 4) open?: " + percTest.isOpen(2, 4));
-		percTest.open(2, 4);
-		System.out.println("Is (2, 4) open?: " + percTest.isOpen(2, 4));
-		percTest.printSites(n, "Result:");
-*/
-        int count = 0;
-		int row = 3;
-		int col = 3;
-		for (int i = 0; i < row; i++) {
-		    for (int j = 0; j < col; j++) {
-				count = i * i + j;
-			    System.out.println("Count: " + count);
+		for (int i = 0; i < (this.n * this.n); i++) {
+		    if (siteStatus[i] == 1) {
+			    numberOfOpenSites++;
 			}
 		}
-		System.out.println("Index: " + count);
+		return numberOfOpenSites;
+	}
+
+	// method percolates: does the system percolate?
+	public boolean percolates() {
+	    // a system can be said to percolate if the virtual top site is connected to the virtual bottom site
+		return grid.connected(0, siteStatus.length + 1);
+	}
+
+	// method main: optional to test the code
+	public static void main(String[] args) {
+	    int n = 0;
+		n = Integer.parseInt(args[0]);
+	    Percolation percTest = new Percolation(n);
+		// keep opening sites and checking if the system percolates
+		while (!percTest.percolates()) {
+		    percTest.open(StdRandom.uniform(1, n + 1), StdRandom.uniform(1, n + 1));
+		}
+		double numerator = (double) percTest.numberOfOpenSites();
+		double denominator = (double) (n*n);
+		double percThres = numerator/denominator;
+		System.out.println("Percolation threshold i.e. numberOfOpenSites/totalNumberOfSites:\t" + percThres);
 	}
 	
 	// print sites
@@ -89,23 +136,28 @@ public class Percolation {
 	    // print message
 		System.out.println(desc);
 		// print top site
-		System.out.println("Top site: " + grid.find(0));
+		System.out.print("Top site: " + grid.find(0) + "\t\t");
+		// print bottom site
+		System.out.println("Bottom site: " + grid.find(n*n + 1));
 		// print all sites
 		System.out.print("Sites:\t");
 		for (int i = 1; i < (n*n + 1); i++) {
 			System.out.print(grid.find(i) + "\t");
 		}
 		System.out.println();
-		// site status
-		System.out.print("Status:\t");
+		// site map
+		System.out.print("Map:\t");
 		for (int i = 0; i < n; i++) {
 		    for (int j = 0; j < n; j++) {
-			    System.out.print(siteStatus[i][j] + "\t");
+			    System.out.print(siteMap[i][j] + "\t");
 			}
 		}
 		System.out.println();
-		// print bottom site
-		System.out.println("Bottom site: " + grid.find(n*n + 1));
+		// site status
+		System.out.print("Status:\t");
+		for (int i = 0; i < n*n; i++) {
+		    System.out.print(siteStatus[i] + "\t");
+		}
 		System.out.println();
 	}
 }
---
+++ assignments/percolation/src/PercolationStats.java
@@ -0,0 +1,85 @@
+/*
+** AUTHOR:	rohidesh
+** PROBLEM:	http://coursera.cs.princeton.edu/algs4/assignments/percolation.html
+** DESC:	Program to get the PercolationStats.
+*/
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class PercolationStats {
+    private int n;
+	private int trials;
+	// percolation thresholds
+	private double[] pt;
+	private double numerator;
+	private double denominator;
+	private double mean = 0.0;
+	private double stddev = 0.0;
+	private double confidenceLo = 0.0;
+	private double confidenceHi = 0.0;
+    // constructor: perform trial experiments on an n-by-n grid
+	public PercolationStats(int n, int trials) {
+	    // check if n and trials are valid
+		if (n <= 0 || trials <= 0) {
+		    throw new IllegalArgumentException("Value of n, trials " + n + ", " + trials + " not acceptable.");
+		}
+		this.n = n;
+		this.trials = trials;
+		denominator = (double) n*n;
+		// initialize pt
+		pt = new double[trials];
+		// run trials
+		for (int i = 0; i < trials; i++) {
+		    Percolation p = new Percolation(n);
+		    // keep opening sites and checking if the system percolates
+		    while (!p.percolates()) {
+		        p.open(StdRandom.uniform(1, n + 1), StdRandom.uniform(1, n + 1));
+		    }
+			numerator = (double) p.numberOfOpenSites();
+			pt[i] = numerator/denominator;
+			//System.out.println(pt[i] + "\t");
+		}
+	}
+	
+	// method mean: sample mean of percolation threshold
+	public double mean() {
+	    for (int i = 0; i < pt.length; i++) {
+		    mean += pt[i];
+		}
+		mean /= this.trials;
+	    return mean;
+	}
+	
+	// method stddev: sample standard deviation of percolation threshold
+	public double stddev() {
+	    for (int i = 0; i < pt.length; i++) {
+		    stddev += (pt[i] - this.mean())*(pt[i] - this.mean());
+		}
+	    stddev /= (this.trials - 1);
+		stddev = Math.sqrt(stddev);
+	    return stddev;
+	}
+	
+	// method confidenceLo: low endpoint of 95% confidence interval
+	public double confidenceLo() {
+	    confidenceLo = this.mean() - ((1.96 * this.stddev())/(Math.sqrt(this.trials)));
+	    return confidenceLo;
+	}
+	
+	// method confidenceHi: high endpoint of 95% confidence interval
+	public double confidenceHi() {
+	    confidenceHi = this.mean() + ((1.96 * this.stddev())/(Math.sqrt(this.trials)));
+	    return confidenceHi;
+	}
+	
+	// method main: for testing
+	public static void main(String[] args) {
+	    int n, trials;
+		n = Integer.parseInt(args[0]);
+		trials = Integer.parseInt(args[1]);
+	    PercolationStats percStats = new PercolationStats(n, trials);
+		System.out.println("mean\t\t\t\t=\t" + percStats.mean());
+		System.out.println("stddev\t\t\t\t=\t" + percStats.stddev());
+		System.out.println("95% confidence interval\t\t=\t[" + percStats.confidenceLo() + ", " + percStats.confidenceHi() + "]");
+	}
+}
